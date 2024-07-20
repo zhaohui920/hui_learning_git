b9c633342f1a482147696f412042468c5ee4c625
小赵 <xiaozhao@xiaozhaodeAir.lan>
2024-07-20 09:22:07 +0800

    first debug test

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main(){
+    string test_w = "0";
+    cout << test_w;
+    test_w = "1";
+    cout << test_w;
+    cout << endl;
+    return 0;
+}
+
